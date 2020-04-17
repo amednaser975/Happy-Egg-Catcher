fc28a2d8e57dbf041e9ea27d551441e480f6cf65
amednaser975 <amednaser975@gmail.com>
2020-04-17 12:05:23 +0200

    Updates Part2

--- js/guide.js
+++ js/guide.js
@@ -1,177 +1,261 @@
-// global variables
-
-var container = $("#container"),
-  containerHeight = container.height(),
-  content = $("#content"),
-  stateBar = $("#statusBar"),
-  scoreSpan = $("#score"),
-  lifeSpan = $("#life"),
-  hen = $(".hen"),
-  eggs = $(".egg"),
-  egg1 = $("#egg1"),
-  egg2 = $("#egg2"),
-  egg3 = $("#egg3"),
-  eggInitialPosition = parseInt(eggs.css("top")), // get eggs the beginning point
-  egg_height = eggs.height(),
+var game = 0,
+  gameId = 0,
+  container = document.getElementById("container"),
+  gameStart = document.getElementById("gameStart"),
+  playBtn = document.getElementById("playBtn"),
+  gameOver = document.getElementById("gameOver"),
+  playAgainBtn = document.getElementById("playAgainBtn"),
   eggCurrentPosition = 0,
-  eggTop = 0,
-  brokenEggNum = 0,
-  basket = $("#basket"),
-  basketHeight = basket.height(),
-  basketTop = containerHeight - basketHeight,
-  basketScore = $("#basketScore"),
-  floor = $("#floor"),
-  //game control variables
-  gameStart = $("#gameStart"),
-  play = $("#play"),
-  gameOverWindow = $("#gameOver"),
-  gameOverScore = $("#gameOverScore"),
-  playAgain = $("#playAgain"),
-  // sound effects variables
-  playSound = new Audio(),
-  scoreSound = new Audio(),
-  loserSound = new Audio(),
-  // reset some variables values
-  score = 0, //the beginning score
-  life = 10, //the beginning num of lifes
-  speed = 2, //the beginning speed
-  maxSpeed = 15, // max speed
-  theGame = 0,
-  animId = 0;
+  //eggtest1 = $("#egg1"),
+  //eggtest2 = $("#egg2"),
+  //eggtest3 = $("#egg3"),
+  //eggs = $(".egg"),
+  eggs = document.getElementsByClassName("egg"),
+  egg1 = document.getElementById("egg1"),
+  egg2 = document.getElementById("egg2"),
+  egg3 = document.getElementById("egg3"),
+  brokenEgg1 = document.getElementById("brokenEgg1"),
+  brokenEgg2 = document.getElementById("brokenEgg2"),
+  brokenEgg3 = document.getElementById("brokenEgg3"),
+  //floor = $("#floor"),
+  floor = document.getElementById("floor"),
+  speed = 4,
+  maxSpeed = 15;
+//InitialPostionOfEgg = parseInt(eggs.css("top")),
+//InitialPostionOfEgg = Number(eggs.style.top),
+InitialPostionOfEgg = 40;
+(brokenEggNumber = 0),
+  (life = Number(document.getElementById("life").innerHTML)),
+  (score = Number(document.getElementById("score").innerHTML)),
+  (currentScore = Number(document.getElementById("currentScore").innerHTML)),
+  //containerHeight = $("#container").height(),
+  (containerHeight = document
+    .getElementById("container")
+    .getBoundingClientRect().height),
+  //basket = $("#basket"),
+  (basket = document.getElementById("basket")),
+  (basketHeight = basket.getBoundingClientRect().height),
+  (floorTop = containerHeight - basketHeight),
+  (playMusic = new Audio("Music/Dndnha.Com.Cairokee.Teer.Bina.Y3am.mp3")),
+  (scoreMusic = new Audio("Music/score.mp3")),
+  (gameOverMusic = new Audio("Music/game over.mp3"));
 
-// get src of sound effects
-playSound.src = "sounds/happy chicken.mp3";
-scoreSound.src = "sounds/score.mp3";
-loserSound.src = "sounds/game over.mp3";
-// make sound continue during the playing
-function enableLoop() {
-  playSound.loop = true;
+// Main Function Running The Game
+game = function() {
+  // Playing Music
+  playMusic.play();
+  playMusicLoop();
+
+  // First Egg
+  if (checkEggCrashFloor(egg1) || insertEggIntoBasket(egg1)) {
+    //if (false) {
+    setEggToInitialPosition(egg1);
+  } else {
+    dropEggs1(egg1);
+  }
+  // Second Egg
+  if (checkEggCrashFloor(egg2) || insertEggIntoBasket(egg2)) {
+    setEggToInitialPosition(egg2);
+  } else {
+    dropEggs1(egg2);
+  }
+  // Third Egg
+  if (checkEggCrashFloor(egg3) || insertEggIntoBasket(egg3)) {
+    setEggToInitialPosition(egg3);
+  } else {
+    dropEggs1(egg3);
+  }
+
+  if (life > 0) {
+    gameId = requestAnimationFrame(game); //Recursion
+    //gameId = setInterval(theGame, 100);
+  } else {
+    cancelAnimationFrame(gameId); // Stop The Game
+    //clearInterval(theGame);
+    gameOver.classList.add("d-block"); // Show Game Over Box
+    container.style.display = "none"; // Hide The Game Components
+    playMusic.pause(); // Stop Game Music
+    gameOverMusic.play(); // Play Game Over Music
+  }
+};
+
+// Music During Playing Game Function
+function playMusicLoop() {
+  playMusic.loop = true;
 }
 
-// start lifes num
-lifeSpan.text(life);
+// Start The Game By Clicking on Play Button
+function runGame() {
+  gameStart.style.display = "none";
+  game_id = requestAnimationFrame(game);
+}
+playBtn.addEventListener("click", runGame);
 
-// move basket with mouse move
-
-$(document).mousemove(function(e) {
-  basket.css("left", e.pageX);
+//Start The Game again By Clicking on Play Again Button
+playAgainBtn.addEventListener("click", function() {
+  location.reload();
 });
 
-// moving eggs down and set it in theGame function blow
-function eggDown(egg) {
-  eggCurrentPosition = parseInt(egg.css("top"));
-  egg.css("top", eggCurrentPosition + speed);
+// Move Basket with Mouse
+document.body.addEventListener("mousemove", function(e) {
+  basket.style.left = String(e.clientX) + "px";
+  //basket.css("left", e.clientX);
+});
+
+// Moving Eggs Down
+function dropEggs1(egg) {
+  //eggCurrentPosition = parseInt(egg.css("top")) + speed;
+  //egg.css("top", eggCurrentPosition);
+  eggCurrentPosition = egg.offsetTop + speed;
+  egg.style.top = String(eggCurrentPosition) + "px";
 }
 
-// show broken eggs when reach the floor & lost one life
-function eggOnFloor(egg) {
-  if (collision(egg, floor)) {
-    showBrokenEgg(egg); // show broken eggs function defined blow
-    lostLife(); // lose life every broken egg function defined blow
-    return true;
+// Collision Function => Checking if 2 elements are collided or not
+function collision(element1, element2) {
+  var element1Left = Math.round(element1.getBoundingClientRect().left),
+    element1Top = Math.round(element1.getBoundingClientRect().top),
+    element1FullHeight = Math.round(element1.getBoundingClientRect().height),
+    element1FullWidth = Math.round(element1.getBoundingClientRect().width),
+    pointXElement1 = element1Left + element1FullWidth,
+    pointYElement1 = element1Top + element1FullHeight;
+
+  var element2Left = Math.round(element2.getBoundingClientRect().left),
+    element2Top = Math.round(element2.getBoundingClientRect().top),
+    element2FullHeight = Math.round(element2.getBoundingClientRect().height),
+    element2FullWidth = Math.round(element2.getBoundingClientRect().width),
+    pointXElement2 = element2Left + element2FullWidth,
+    pointYElement2 = element2Top + element2FullHeight;
+
+  if (
+    pointYElement1 < element2Top ||
+    element1Top > pointYElement2 ||
+    pointXElement1 < element2Left ||
+    element1Left > pointXElement2
+  ) {
+    //console.log("Never");
+    return false; // The 2 Elements Didn't Collided
+  } else {
+    return true; // The 2 Elements have Collided
   }
-  return false;
 }
 
-// reback the start position of eggs and set it in theGame function blow
-function startEgg(egg) {
-  egg.css("top", eggInitialPosition);
+/*function collision (element1, element2)
+{
+  var element1Left = Math.round(element1.getBoundingClientRect().left),
+      element1Top = Math.round(element1.getBoundingClientRect().top),
+      element1FullHeight = Math.round(element1.getBoundingClientRect().height),
+      element1FullWidth = Math.round(element1.getBoundingClientRect().height),
+      pointXElement1 = element1Left + element1FullWidth,
+      pointYElement1 = element1Top + element1FullHeight;
+
+  var element2Left = Math.round(element2.getBoundingClientRect().left),
+      element2Top = Math.round(element2.getBoundingClientRect().top),
+      element2FullHeight = Math.round(element2.getBoundingClientRect().height),
+      element2FullWidth = Math.round(element2.getBoundingClientRect().height),
+      pointXElement2 = element2Left + element2FullWidth,
+      pointYElement2 = element2Top + element2FullHeight;
+    
+  if (pointYElement1 < element2Top || element1Top > pointYElement2 ||
+      pointXElement1 < element2Left || element1Left > pointXElement2) {
+
+        console.log("Never");
+      return false; // The 2 Elements Didn't Collided
+
+  } else {
+
+     return true; // The 2 Elements have Collided
+
+  }
+    
+}*/
+/*function collision($element1, $element2) {
+  var element1Left = $element1.offset().left,
+    element1Top = $element1.offset().top,
+    element1FullHeight = $element1.outerHeight(true),
+    element1FullWidth = $element1.outerWidth(true),
+    pointXElement1 = element1Left + element1FullWidth,
+    pointYElement1 = element1Top + element1FullHeight;
+
+  var element2Left = $element2.offset().left,
+    element2Top = $element2.offset().top,
+    element2FullHeight = $element2.outerHeight(true),
+    element2FullWidth = $element2.outerWidth(true),
+    pointXElement2 = element2Left + element2FullWidth,
+    pointYElement2 = element2Top + element2FullHeight;
+
+  if (
+    pointYElement1 < element2Top ||
+    element1Top > pointYElement2 ||
+    pointXElement1 < element2Left ||
+    element1Left > pointXElement2
+  ) {
+    return false; // The 2 Elements Didn't Collided
+  } else {
+    return true; // The 2 Elements have Collided
+  }
+}*/
+
+// Check Egg Crash into Floor or Not Function
+function checkEggCrashFloor(egg) {
+  if (collision(egg, floor)) {
+    showBrokenEgg(egg);
+    decrementLife();
+    return true;
+  } else {
+    return false;
+  }
 }
 
-// show & hide broken egg function
-function showBrokenEgg(egg) {
-  brokenEggNum = egg.attr("data-brokenEgg");
-  $("#brokenEgg" + brokenEggNum).show(1, function() {
-    setTimeout(function() {
-      $("#brokenEgg" + brokenEggNum).hide(); // hide broken egg after 600 ms
-    }, 600);
-  });
-}
-
-// lost one life function
-
-function lostLife() {
-  life--; // lose life every broken egg
-  lifeSpan.text(life); // updat lifes num in life span
-}
-
-// when the basket take an egg
-function eggsInBasket(egg) {
+// Check Egg Insert into Floor or Not Function
+function insertEggIntoBasket(egg) {
   if (collision(egg, basket)) {
-    eggTop = parseInt(egg.css("top"));
-    if (eggTop < basketTop) {
-      scoreUp(); // updat the scor function defined blow
-      scoreSound.play(); // play scoresound function defined
+    //var eggTop = Number(egg.css("top"));
+    var eggTop = egg.offsetTop;
+    if (eggTop < floorTop) {
+      updateScore();
+      scoreMusic.play(); // Play Score Music
       return true;
     }
   }
   return false;
 }
 
-// Update the score function
-function scoreUp() {
-  score++; // increment score
-  if (score % 10 === 0 && speed <= maxSpeed) {
-    speed++; //increment speed
-  }
-  scoreSpan.text(score); // update text of score span
-  basketScore.text(score); // update text of score on basket
+// Set Egg to Its Initial Position
+function setEggToInitialPosition(egg) {
+  //egg.css("top", InitialPostionOfEgg);
+  egg.style.top = String(InitialPostionOfEgg) + "px";
 }
 
-// stop the game when losing
-function stopTheGame() {
-  cancelAnimationFrame(animId); // stop the moving of eggs
-  $(".brokenEgg").hide(); // hide the broken eggs
-  gameOverScore.text(score); // show the score in game over popup
-  playSound.pause(); //stop playing sound
-  loserSound.play(); // play game over sound
-  gameOverWindow.show(); // show game over popup
-  content.hide(); // hide all screen items
+// Show Broken Egg Function
+function showBrokenEgg(egg) {
+  //brokenEggNumber = $(egg).attr("data-brokenEgg");
+  brokenEggNumber = egg.getAttribute("data-brokenEgg");
+  var brokenEggId = "brokenEgg" + brokenEggNumber;
+  document.getElementById(brokenEggId).style.display = "block";
+  hideBrokenEgg(brokenEggId);
 }
 
-// reload the game when press play again
-playAgain.click(function() {
-  location.reload();
-});
+// Hide Broken Egg Function
+function hideBrokenEgg(brokenEggId) {
+  setTimeout(function() {
+    document.getElementById(brokenEggId).style.display = "none";
+  }, 1000);
+}
 
-//  game animation and effects function (theGame)
-var theGame = function() {
-  playSound.play(); // for play sound
-  enableLoop(); // looping play sound function defined above
-  // first egg
-  // reback eggs when reach the floor or catch by basket
-  if (eggOnFloor(egg1) || eggsInBasket(egg1)) {
-    startEgg(egg1);
-  } else {
-    // moving eggs down
-    eggDown(egg1); // function defined above
+// Decrement Life Function
+function decrementLife() {
+  life--;
+  document.getElementById("life").innerHTML = life;
+}
+
+// Update Score Function
+function updateScore() {
+  score++, currentScore++;
+  if (score % 20 == 0 && speed <= maxSpeed) {
+    speed++;
   }
+  document.getElementById("score").innerHTML = score;
 
-  //scond egg
-  // reback eggs when reach the floor
-  if (eggOnFloor(egg2) || eggsInBasket(egg2)) {
-    startEgg(egg2);
-  } else {
-    // move eggs down
-    eggDown(egg2);
-  }
-
-  // third egg
-  // reback eggs when reach the floor
-  if (eggOnFloor(egg3) || eggsInBasket(egg3)) {
-    startEgg(egg3);
-  } else {
-    // move eggs down
-    eggDown(egg3);
-  }
-
-  if (life > 0) {
-    game_id = requestAnimationFrame(theGame); //repeat the moving
-  } else stopTheGame(); // stop when losing all lifes function defined above
-};
-
-// start game when press play btn
-play.click(function() {
-  gameStart.hide(); //hide the popup
-  game_id = requestAnimationFrame(theGame); //start game
-});
+  document.getElementById("currentScore").innerHTML = currentScore;
+  document.getElementById("gameOverScore").innerHTML = currentScore;
+}
--- js/main.js
+++ js/main.js
@@ -1,170 +1,123 @@
-var theGame = 0,
-    gameId = 0,
-    container = document.getElementById('container'),
-    gameStart = document.getElementById('gameStart'),
-    playBtn = document.getElementById('playBtn'),
-    gameOver = document.getElementById('gameOver'),
-    playAgainBtn = document.getElementById('playAgainBtn'),
-    eggCurrentPosition = 0,
-    eggtest1 = $('#egg1'),
-    eggtest2 = $('#egg2'),
-    eggtest3 = $('#egg3'),
-    eggs = $('.egg'),
-    egg1 = document.getElementById('egg2'),
-    egg2 = document.getElementById('egg2'),
-    egg3 = document.getElementById('egg3'),
-    brokenEgg1 = document.getElementById('brokenEgg1'),
-    brokenEgg2 = document.getElementById('brokenEgg2'),
-    brokenEgg3 = document.getElementById('brokenEgg3'),
-    floor =  $('#floor'),
-    speed = 4,
-    maxSpeed = 15,
-    InitialPostionOfEgg = parseInt(eggs.css('top')),
-    brokenEggNumber = 0,
-    life = Number(document.getElementById('life').innerHTML),
-    score = Number(document.getElementById('score').innerHTML),
-    currentScore = Number(document.getElementById('currentScore').innerHTML),
-    containerHeight = $('#container').height(),
-    basket = $('#basket'),
-    basketHeight = basket.height(),
-    basketTop = containerHeight - basketHeight,
-    playMusic = new Audio("Music/Dndnha.Com.Cairokee.Teer.Bina.Y3am.mp3"),
-    scoreMusic = new Audio("Music/score.mp3"),
-    gameOverMusic = new Audio("Music/game over.mp3");
-
+var game = 0,
+  gameId = 0,
+  container = document.getElementById("container"),
+  gameStart = document.getElementById("gameStart"),
+  playBtn = document.getElementById("playBtn"),
+  gameOver = document.getElementById("gameOver"),
+  playAgainBtn = document.getElementById("playAgainBtn"),
+  eggCurrentPosition = 0,
+  eggs = document.getElementsByClassName('egg'),
+  egg1 = document.getElementById("egg1"),
+  egg2 = document.getElementById("egg2"),
+  egg3 = document.getElementById("egg3"),
+  brokenEgg1 = document.getElementById("brokenEgg1"),
+  brokenEgg2 = document.getElementById("brokenEgg2"),
+  brokenEgg3 = document.getElementById("brokenEgg3"),
+  floor = document.getElementById("floor"),
+  speed = 4,
+  maxSpeed = 15;
+  InitialPostionOfEgg = 40;
+  brokenEggNumber = 0,
+  life = Number(document.getElementById("life").innerHTML),
+  score = Number(document.getElementById("score").innerHTML),
+  currentScore = Number(document.getElementById("currentScore").innerHTML),
+  containerHeight = document.getElementById('container').getBoundingClientRect().height,
+  basket = document.getElementById("basket"),
+  basketHeight = basket.getBoundingClientRect().height,
+  floorTop = containerHeight - basketHeight,
+  playMusic = new Audio("Music/Dndnha.Com.Cairokee.Teer.Bina.Y3am.mp3"),
+  scoreMusic = new Audio("Music/score.mp3"),
+  gameOverMusic = new Audio("Music/game over.mp3");
 
 // Main Function Running The Game
-theGame = function () {
-  
+game = function() {
   // Playing Music
   playMusic.play();
   playMusicLoop();
 
   // First Egg
-  if (checkEggCrashFloor(eggtest1) || insertEggIntoBasket(eggtest1)) {
-
-    setEggToInitialPosition(eggtest1);
+  if (checkEggCrashFloor(egg1) || insertEggIntoBasket(egg1)) {
+  //if (false) {
+    setEggToInitialPosition(egg1);
   } else {
-    
-    dropEggs1(eggtest1);
+    dropEggs1(egg1);
   }
   // Second Egg
-  if (checkEggCrashFloor(eggtest2) || insertEggIntoBasket(eggtest2)) {
-
-    setEggToInitialPosition(eggtest2);
+  if (checkEggCrashFloor(egg2) || insertEggIntoBasket(egg2)) {
+    setEggToInitialPosition(egg2);
   } else {
-    
-    dropEggs1(eggtest2);
+    dropEggs1(egg2);
   }
   // Third Egg
-  if (checkEggCrashFloor(eggtest3) || insertEggIntoBasket(eggtest3)) {
-
-    setEggToInitialPosition(eggtest3);
+  if (checkEggCrashFloor(egg3) || insertEggIntoBasket(egg3)) {
+    setEggToInitialPosition(egg3);
   } else {
-    
-    dropEggs1(eggtest3);
+    dropEggs1(egg3);
   }
 
-  if (life > 0)
-  {
-    gameId = requestAnimationFrame(theGame); //Recursion
+  if (life > 0) {
+    gameId = requestAnimationFrame(game); //Recursion
     //gameId = setInterval(theGame, 100);
   } else {
-
     cancelAnimationFrame(gameId); // Stop The Game
     //clearInterval(theGame);
-    gameOver.classList.add('d-block'); // Show Game Over Box
-    container.style.display = 'none'; // Hide The Game Components
+    gameOver.classList.add("d-block"); // Show Game Over Box
+    container.style.display = "none"; // Hide The Game Components
     playMusic.pause(); // Stop Game Music
     gameOverMusic.play(); // Play Game Over Music
   }
 };
 
 // Music During Playing Game Function
-function playMusicLoop()
-{
+function playMusicLoop() {
   playMusic.loop = true;
 }
 
 // Start The Game By Clicking on Play Button
-function runGame ()
-{
-  gameStart.style.display = 'none';
-  game_id = requestAnimationFrame(theGame);
+function runGame() {
+  gameStart.style.display = "none";
+  game_id = requestAnimationFrame(game);
 }
-playBtn.addEventListener('click', runGame);
+playBtn.addEventListener("click", runGame);
 
 //Start The Game again By Clicking on Play Again Button
-playAgainBtn.addEventListener('click', function ()
-{
+playAgainBtn.addEventListener("click", function() {
   location.reload();
 });
 
-
-
 // Move Basket with Mouse
-document.body.addEventListener('mousemove', function(e) {
-
-  //basket.style.left = String(e.clientX) + 'px';
-  basket.css('left', e.clientX);
+document.body.addEventListener("mousemove", function(e) {
+  basket.style.left = String(e.clientX) + 'px';
 });
 
 // Moving Eggs Down
-function dropEggs1(egg)
-{
-  eggCurrentPosition = parseInt(egg.css('top')) + speed;
-  egg.css('top', eggCurrentPosition);
-  //egg.style.top = String(eggCurrentPosition) + 'px';
+function dropEggs1(egg) {
+
+  eggCurrentPosition = egg.offsetTop + speed;
+  egg.style.top = String(eggCurrentPosition) + 'px';
 }
 
 // Collision Function => Checking if 2 elements are collided or not
-/*function collision (element1, element2)
+function collision (element1, element2)
 {
-  var element1Left = element1.offsetLeft,
-      element1Top = element1.offsetTop,
-      element1FullHeight = element1.offsetHeight,
-      element1FullWidth = element1.offsetWidth;
-      pointXElement1 = element1Left + element1FullWidth,
-      pointYForElement1 = element1Top + element1FullHeight;
-
-  var element2Left = element2.offsetLeft,
-      element2Top = element2.offsetTop,
-      element2FullHeight = element2.offsetHeight,
-      element2FullWidth = element2.offsetWidth,
-      pointXElement2 = element2Left + element2FullWidth,
-      pointYElement2 = element2Top + element2FullHeight;
-
-  if (pointYForElement1 < element2Top || element1Top > pointYElement2 ||
-      pointXElement1 < element2Left || element1Left > pointXElement2) {
-
-      return false; // The 2 Elements Didn't Collided
-
-  } else {
-
-     return true; // The 2 Elements have Collided
-
-  }
-    
-}*/
-function collision ($element1, $element2)
-{
-  var element1Left = $element1.offset().left,
-      element1Top = $element1.offset().top,
-      element1FullHeight = $element1.outerHeight(true),
-      element1FullWidth = $element1.outerWidth(true),
+  var element1Left = Math.round(element1.getBoundingClientRect().left),
+      element1Top = Math.round(element1.getBoundingClientRect().top),
+      element1FullHeight = Math.round(element1.getBoundingClientRect().height),
+      element1FullWidth = Math.round(element1.getBoundingClientRect().width),
       pointXElement1 = element1Left + element1FullWidth,
       pointYElement1 = element1Top + element1FullHeight;
 
-  var element2Left = $element2.offset().left,
-      element2Top = $element2.offset().top,
-      element2FullHeight = $element2.outerHeight(true),
-      element2FullWidth = $element2.outerWidth(true),
+  var element2Left = Math.round(element2.getBoundingClientRect().left),
+      element2Top = Math.round(element2.getBoundingClientRect().top),
+      element2FullHeight = Math.round(element2.getBoundingClientRect().height),
+      element2FullWidth = Math.round(element2.getBoundingClientRect().width),
       pointXElement2 = element2Left + element2FullWidth,
       pointYElement2 = element2Top + element2FullHeight;
-
+    
   if (pointYElement1 < element2Top || element1Top > pointYElement2 ||
       pointXElement1 < element2Left || element1Left > pointXElement2) {
 
+        //console.log("Never");
       return false; // The 2 Elements Didn't Collided
 
   } else {
@@ -174,49 +127,42 @@ function collision ($element1, $element2)
   }
     
 }
-// Check Egg Crash into Floor or Not Function
-function checkEggCrashFloor (egg) 
-{
-  if(collision(egg, floor)) {
 
+// Check Egg Crash into Floor or Not Function
+function checkEggCrashFloor(egg) {
+  if (collision(egg, floor)) {
     showBrokenEgg(egg);
     decrementLife();
     return true;
-  
   } else {
-
-  return false;
+    return false;
   }
 }
 
 // Check Egg Insert into Floor or Not Function
-function insertEggIntoBasket (egg)
-{
-  
-  if(collision(egg, basket)) {
-    
-    var eggTop = parseInt(egg.css('top'));
+function insertEggIntoBasket(egg) {
+  if (collision(egg, basket)) {
 
-    if (eggTop < basketTop) {
-
+    var eggTop = egg.offsetTop;
+    if (eggTop < floorTop) {
       updateScore();
       scoreMusic.play(); // Play Score Music
       return true;
-
     }
   }
   return false;
 }
 
 // Set Egg to Its Initial Position
-function setEggToInitialPosition (egg) {
+function setEggToInitialPosition(egg) {
 
-  egg.css('top', InitialPostionOfEgg);
+  egg.style.top = String(InitialPostionOfEgg) + 'px';
 }
 
 // Show Broken Egg Function
 function showBrokenEgg(egg) {
-  brokenEggNumber = $(egg).attr("data-brokenEgg");
+
+  brokenEggNumber = egg.getAttribute("data-brokenEgg");
   var brokenEggId = "brokenEgg" + brokenEggNumber;
   document.getElementById(brokenEggId).style.display = "block";
   hideBrokenEgg(brokenEggId);
@@ -226,26 +172,24 @@ function showBrokenEgg(egg) {
 function hideBrokenEgg(brokenEggId) {
   setTimeout( function () 
   {
-    brokenEggId.style.display = "none";
+    document.getElementById(brokenEggId).style.display = "none";
   }, 1000);
 }
 
 // Decrement Life Function
-function decrementLife()
-{
+function decrementLife() {
   life--;
-  document.getElementById('life').innerHTML = life;
+  document.getElementById("life").innerHTML = life;
 }
 
 // Update Score Function
-function updateScore()
-{
+function updateScore() {
   score++, currentScore++;
-  if (score % 20 == 0 && speed <= maxSpeed)
-  {
+  if (score % 20 == 0 && speed <= maxSpeed) {
     speed++;
-  } 
-  document.getElementById('score').innerHTML = score;
-  document.getElementById('currentScore').innerHTML = currentScore;
-  document.getElementById('gameOverScore').innerHTML = currentScore;
+  }
+  document.getElementById("score").innerHTML = score;
+
+  document.getElementById("currentScore").innerHTML = currentScore;
+  document.getElementById("gameOverScore").innerHTML = currentScore;
 }
